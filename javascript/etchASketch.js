d285bf15326e732c1d7156b6bb6218194d7b5377
George Ciesinski <george.ciesin@gmail.com>
2022-09-23 16:49:00 -0400

    Created Functional Grid - Created 16x16 grid - Added some controls to change size - Added color selection controls

--- javascript/etchASketch.js
+++ javascript/etchASketch.js
@@ -1,2 +1,29 @@
-// Select grid div and generate grid max 100x100
-const grid = document.querySelector("#grid");
+const grid = document.querySelector("#grid");  // Grid parent div
+
+let currentSize = 16;  // Starting & current grid size
+let newSize = 16;  // New Grid size for resize
+
+const gridSelector = document.querySelector("#grid-slider");  // Get grid size slider
+
+gridSelector.oninput = function() {
+    gridSize = this.value;
+    console.log(gridSize);
+}
+
+function createGrid(){
+    grid.replaceChildren(); // Removes any child nodes
+
+    // Create Rows
+    for (let i = 1; i <= gridSize; i++) {
+        const row = document.createElement('div');
+        // Create Columns
+        for (let j = 1; j <= gridSize; j++) {
+            const column = document.createElement('div');
+            column.textContent = j;
+            row.appendChild(column);
+        }
+        grid.appendChild(row);
+    }
+}
+
+createGrid();
