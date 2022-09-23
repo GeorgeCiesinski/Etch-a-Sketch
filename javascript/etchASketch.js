40a004b278433c978a671d8183bba21d26ff0a63
George Ciesinski <george.ciesin@gmail.com>
2022-09-23 17:44:15 -0400

    Added Grid Resize Feature - Grid now generates at 16x16 - Slider allows any size up to 100x100 - Clear and Resize button recreates grid

--- javascript/etchASketch.js
+++ javascript/etchASketch.js
@@ -1,29 +1,43 @@
+// Get Elements
 const grid = document.querySelector("#grid");  // Grid parent div
+const gridSelector = document.querySelector("#grid-slider");  // Get grid size slider
+const btnResize = document.querySelector("#resize");
+const currentGridPreview = document.querySelector("#current-grid");
+const newGridLabel = document.querySelector("#new-grid-label");
+const newGridPreview = document.querySelector("#new-grid");
 
-let currentSize = 16;  // Starting & current grid size
+// Event Listeners
+gridSelector.addEventListener("input", resizePreview);
+btnResize.addEventListener("click", createGrid);
+
 let newSize = 16;  // New Grid size for resize
 
-const gridSelector = document.querySelector("#grid-slider");  // Get grid size slider
-
-gridSelector.oninput = function() {
-    gridSize = this.value;
-    console.log(gridSize);
+function resizePreview(value) {
+    newSize = this.value;
+    newGridLabel.textContent = "New Grid Size:";
+    newGridPreview.textContent = newSize + " x " + newSize;
 }
 
 function createGrid(){
+    console.log("Created new grid");
+    console.log(newSize);
+
     grid.replaceChildren(); // Removes any child nodes
 
     // Create Rows
-    for (let i = 1; i <= gridSize; i++) {
+    for (let i = 1; i <= newSize; i++) {
         const row = document.createElement('div');
         // Create Columns
-        for (let j = 1; j <= gridSize; j++) {
+        for (let j = 1; j <= newSize; j++) {
             const column = document.createElement('div');
-            column.textContent = j;
             row.appendChild(column);
         }
         grid.appendChild(row);
     }
+
+    currentGridPreview.textContent = newSize + " x " + newSize;  // Update Current Grid Size
+    newGridLabel.textContent = "";  // Hide New Grid Size info
+    newGridPreview.textContent = "";
 }
 
 createGrid();
